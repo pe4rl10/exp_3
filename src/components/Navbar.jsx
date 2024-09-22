6b344f4a1e4cf433d42b63bfd17592fc0b2d303a
Pearl Patel <pearlpatel052@gmail.com>
2024-09-23 00:20:54 +0530

    Add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div>
+            <img src='react.svg' alt='logo'/>
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About us</a></li>
